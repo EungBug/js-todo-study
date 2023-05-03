96a0698234fddb637156d6755b0bb3e1a1d9dc52
EungBug <eungb14@gmail.com>
2023-05-03 23:16:48 +0900

    Add ToDo 모듈화 관련 Comment2

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,10 @@
 import { saveTodoItem, getTodoItems, getTodoCount, changeTodoTaskDone, deleteTodoTaskById } from './core/storage';
 
+/**
+ * storage.js의 함수들을 import하여 UI 컴포넌트에서 기능마다 필요한 메서드 호출 시
+ * storage의 함수들을 호출하고 컴포넌트에서 데이터 변경을 구독 하면 데이터가 변경 시
+ * 해당 콜백 함수를 호출해준다.
+ */
 export default class ToDo {
   constructor() {
     this.observers = [];
