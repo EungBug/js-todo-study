279ab27109b34b1d6eec30d657858736ec2ed124
EungBug <eungb14@gmail.com>
2023-05-07 03:16:01 +0900

    Refactor Timer Component 분리

--- src/Timer.js
+++ src/Timer.js
@@ -2,37 +2,56 @@ import Component from './core/Component';
 
 export default class Timer extends Component {
   render() {
+    const timer = new TimerCircle().el;
     this.el.classList.add('timer');
-    const baseCircle = document.createElement('div');
-    baseCircle.classList.add('circle');
-    const lines = document.createElement('div');
-    lines.classList.add('lines');
+    this.el.append(timer);
+  }
+}
+
+class TimerCircle extends Component {
+  render() {
+    const timeLines = new TimeLines().el;
+    const coverCircle = new CoverCircle().el;
+    this.el.classList.add('circle');
+    this.el.append(timeLines, coverCircle);
+  }
+}
+
+class CoverCircle extends Component {
+  render() {
+    const timerFins = new TimerFins().el;
+    this.el.classList.add('cover-circle');
+    this.el.append(timerFins);
+  }
+}
+
+class TimeLines extends Component {
+  render() {
+    this.el.classList.add('lines');
 
     for (let i = 0; i < 12; i++) {
       const line = document.createElement('div');
       line.classList.add('line');
+
+      // 5분 30도
       line.style.transform = `rotate(${i * 30}deg)`;
 
-      lines.append(line);
+      this.el.append(line);
     }
-    baseCircle.append(lines);
+  }
+}
 
-    const coverCircle = document.createElement('div');
-    coverCircle.classList.add('cover-circle');
-    baseCircle.append(coverCircle);
+class TimerFins extends Component {
+  render() {
+    this.el.classList.add('fins');
 
-    const fins = document.createElement('div');
-    fins.classList.add('fins');
-
+    // 50분 3000개 핀을 만들어 준다.
     for (let i = 1; i <= 3000; i++) {
       const fin = document.createElement('div');
       fin.classList.add('fin');
+      // 1초 0.1도
       fin.style.transform = `rotate(${-i * 0.1}deg)`;
-      fins.append(fin);
+      this.el.append(fin);
     }
-
-    coverCircle.append(fins);
-
-    this.el.append(baseCircle);
   }
 }
